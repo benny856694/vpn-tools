2b934ddf494805aa244ae7b2553aa071271a641b
benny <shenbinsc@qq.com>
2025-10-18 16:44:04 +0800

    add buttons

--- src/App.vue
+++ src/App.vue
@@ -5,52 +5,54 @@
     :class="{ dark: mode }"
     :theme-overrides="appTheme(darkMode)"
   >
-    <n-layout position="absolute">
-      <n-layout-header style="height: 64px; padding: 24px" bordered>
-        <div class="text-right">
-          <n-switch v-model:value="mode" data-cy="dark-mode">
-            <template #checked> Dark </template>
-            <template #unchecked> Light </template>
-            <template #checked-icon>
-              <n-icon>
-                <moon />
-              </n-icon>
-            </template>
-            <template #unchecked-icon>
-              <n-icon>
-                <sun />
-              </n-icon>
-            </template>
-          </n-switch>
-        </div>
-      </n-layout-header>
-      <n-layout
-        class="bg-white dark:bg-gray-800 dark:text-white text-gray-800 text-center h-screen w-screen"
-        has-sider
-        position="absolute"
-        style="top: 64px; bottom: 64px"
-      >
-        <n-layout-sider
-          data-cy="sidebar"
-          class="fixed h-full"
-          collapse-mode="transform"
-          :width="240"
-          :native-scrollbar="false"
-          bordered
-          show-trigger="arrow-circle"
+    <NMessageProvider>
+      <n-layout position="absolute">
+        <n-layout-header style="height: 64px; padding: 24px" bordered>
+          <div class="text-right">
+            <n-switch v-model:value="mode" data-cy="dark-mode">
+              <template #checked> Dark </template>
+              <template #unchecked> Light </template>
+              <template #checked-icon>
+                <n-icon>
+                  <moon />
+                </n-icon>
+              </template>
+              <template #unchecked-icon>
+                <n-icon>
+                  <sun />
+                </n-icon>
+              </template>
+            </n-switch>
+          </div>
+        </n-layout-header>
+        <n-layout
+          class="bg-white dark:bg-gray-800 dark:text-white text-gray-800 text-center h-screen w-screen"
+          has-sider
+          position="absolute"
+          style="top: 64px; bottom: 64px"
         >
-          <n-h2>Layout Sider</n-h2>
-        </n-layout-sider>
-        <n-layout-content :native-scrollbar="false">
-          <router-view class="m-2" />
-        </n-layout-content>
+          <n-layout-sider
+            data-cy="sidebar"
+            class="fixed h-full"
+            collapse-mode="transform"
+            :width="240"
+            :native-scrollbar="false"
+            bordered
+            show-trigger="arrow-circle"
+          >
+            <n-h2>Layout Sider</n-h2>
+          </n-layout-sider>
+          <n-layout-content :native-scrollbar="false">
+            <router-view class="m-2" />
+          </n-layout-content>
+        </n-layout>
       </n-layout>
-    </n-layout>
+    </NMessageProvider>
   </n-config-provider>
 </template>
 
 <script setup lang="ts">
-import { darkTheme, lightTheme } from 'naive-ui'
+import { darkTheme, lightTheme, NMessageProvider } from 'naive-ui'
 import { computed, ref, watchEffect } from 'vue'
 import { appTheme } from '@/lib'
 
--- src/views/VlessUrlMod.vue
+++ src/views/VlessUrlMod.vue
@@ -7,34 +7,78 @@
       </NText>
     </NH2>
     <div class="flex flex-row gap-x-2 m-2 items-center text-left">
-      <NInput
-        v-model:value="orgUrls"
-        type="textarea"
-        placeholder="original vless urls, can contain multiple urls. each url starts with vless://"
-        rows="30"
-        >original url</NInput
-      >
+      <NFlex class="flex-1" justify="end">
+        <NButtonGroup size="small">
+          <n-button
+            type="error"
+            :disabled="orgUrls?.length == 0"
+            @click="orgUrls = ''"
+          >
+            <template #icon>
+              <n-icon><close-round /></n-icon>
+            </template>
+            Clear
+          </n-button>
+        </NButtonGroup>
+        <NInput
+          v-model:value="orgUrls"
+          type="textarea"
+          placeholder="original vless urls, can contain multiple urls. each url starts with vless://"
+          rows="30"
+          >original url</NInput
+        >
+      </NFlex>
       <NButton
         :disabled="loading || orgUrls?.length == 0"
         :loading="loading"
         @click="convert"
         >Convert</NButton
       >
-      <NInput
-        v-model:value="modUrls"
-        type="textarea"
-        placeholder="modified url"
-        rows="30"
-        >modified url</NInput
-      >
+      <NFlex class="flex-1" justify="end">
+        <NButtonGroup size="small">
+          <n-button
+            type="error"
+            :disabled="modUrls?.length == 0"
+            @click="modUrls = ''"
+          >
+            <template #icon>
+              <n-icon><CloseRound /></n-icon>
+            </template>
+            Clear
+          </n-button>
+          <n-button :disabled="modUrls?.length == 0" @click="copyToClipboard">
+            <template #icon>
+              <n-icon><ContentCopyRound /></n-icon>
+            </template>
+            Copy
+          </n-button>
+        </NButtonGroup>
+        <NInput
+          v-model:value="modUrls"
+          type="textarea"
+          placeholder="modified url"
+          rows="30"
+          >modified url</NInput
+        >
+      </NFlex>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { appendGeolocationToVlessNames } from '@/utils/utils'
-import { NInput, NButton, NH2, NText } from 'naive-ui'
 import { ref } from 'vue'
+import {
+  NInput,
+  NButton,
+  NH2,
+  NText,
+  NFlex,
+  useMessage,
+  NButtonGroup
+} from 'naive-ui'
+import { CloseRound, ContentCopyRound } from '@vicons/material'
+import { appendGeolocationToVlessNames } from '@/utils/utils'
+const message = useMessage()
 const orgUrls = ref<string>('')
 const modUrls = ref<string>('')
 const loading = ref<boolean>(false)
@@ -46,6 +90,10 @@ async function convert() {
   } finally {
     loading.value = false
   }
+}
+function copyToClipboard() {
+  navigator.clipboard.writeText(modUrls.value)
+  message.success('Copied to clipboard')
 }
 </script>
 
